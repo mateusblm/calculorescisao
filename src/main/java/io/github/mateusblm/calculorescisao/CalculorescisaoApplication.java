8734ab4b60aa0853da84d59f04b8181e9c5e6591
Mateus Burlamaqui <mateusblm@outlook.com>
2025-03-26 14:10:54 -0300

    Iniciando o projeto, criando os pacotes e a estrutura.

---
+++ src/main/java/io/github/mateusblm/calculorescisao/CalculorescisaoApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateusblm.calculorescisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculorescisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculorescisaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateusblm/calculorescisao/CalculorescisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateusblm.calculorescisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculorescisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
