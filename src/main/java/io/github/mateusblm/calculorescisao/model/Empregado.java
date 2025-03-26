3424c76d52ee1577ab509ada05089f77fb4bbf97
Mateus Burlamaqui <mateusblm@outlook.com>
2025-03-26 15:23:38 -0300

    Criando getters e setters e configurando ID unico.

--- src/main/java/io/github/mateusblm/calculorescisao/model/Empregado.java
+++ src/main/java/io/github/mateusblm/calculorescisao/model/Empregado.java
@@ -1,5 +1,6 @@
 package io.github.mateusblm.calculorescisao.model;
 
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -8,10 +9,61 @@ import java.util.Date;
 
 @Data
 public class Empregado {
-    private String nome;
+
+    @Id
     private String cpf;
+
+    private String nome;
     private LocalDate dataAdmissao;
     private LocalDate dataDemissao;
     private BigDecimal salarioAtual;
     private int numeroDependentes;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(LocalDate dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public LocalDate getDataDemissao() {
+        return dataDemissao;
+    }
+
+    public void setDataDemissao(LocalDate dataDemissao) {
+        this.dataDemissao = dataDemissao;
+    }
+
+    public BigDecimal getSalarioAtual() {
+        return salarioAtual;
+    }
+
+    public void setSalarioAtual(BigDecimal salarioAtual) {
+        this.salarioAtual = salarioAtual;
+    }
+
+    public int getNumeroDependentes() {
+        return numeroDependentes;
+    }
+
+    public void setNumeroDependentes(int numeroDependentes) {
+        this.numeroDependentes = numeroDependentes;
+    }
 }
