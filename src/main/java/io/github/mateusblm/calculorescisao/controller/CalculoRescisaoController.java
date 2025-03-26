489fca5850cc8e62e73ac3a340928fa94e80525c
Mateus Burlamaqui <mateusblm@outlook.com>
2025-03-26 15:24:45 -0300

    Criando a classe controller para receber os requests.

---
+++ src/main/java/io/github/mateusblm/calculorescisao/controller/CalculoRescisaoController.java
@@ -0,0 +1,12 @@
+package io.github.mateusblm.calculorescisao.controller;
+
+import io.github.mateusblm.calculorescisao.service.CalculoRescisaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CalculoRescisaoController {
+
+    @Autowired
+    private CalculoRescisaoService calculoRescisaoService;
+
+
+}
